1ec55b9e85eeaf2a83ca94061fb3e6bcf00de7ce
Clarissa Ardon <clarissa.ardon@crealogix.com>
2022-06-17 00:06:34 +0200

    initial FlatList

--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
-import { Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import React from 'react';
+import { View, FlatList, Text } from 'react-native';
+
+interface MenuItem {
+  name: string;
+  icon: string;
+  component: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'paw',
+    component: 'Animation101Screen'
+  }
+]
 
 const Homescreen = () => {
+
+  const renderMenuItem = ( menuItem: MenuItem ) => {
+    return (
+      <View>
+        <Text>{menuItem.name} - {menuItem.icon}</Text>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Icon name="star-outline" size={50} color="black" />
+    <View style={{ flex: 1 }}>
+      <FlatList 
+        data={ menuItems }
+        renderItem={ ({ item }) => renderMenuItem( item ) }
+        keyExtractor={ (item) => item.name }
+      />
     </View>
   )
 }
