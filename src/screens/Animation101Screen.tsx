3b79008deceab7e085b66cd4d1fffc8cd9ea5d4a
Clarissa Ardon <clarissa.ardon@crealogix.com>
2022-06-19 22:28:06 +0200

    fadeIn fadeOut

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,12 +1,46 @@
 import React, { useRef } from 'react';
-import { Animated, StyleSheet, View } from 'react-native';
+import { Animated, Button, StyleSheet, View } from 'react-native';
 
 export const Animation101Screen = () => {
   const opacity = useRef( new Animated.Value(0.4) ).current;
+  
+  const fadeIn = () => {
+    Animated.timing(
+      opacity, 
+      {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true
+      }
+    ).start();
+  }
+
+  const fadeOut = () => {
+    Animated.timing(
+      opacity, 
+      {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true
+      }
+    ).start();
+  }
 
   return (
     <View style={styles.container}>
-        <Animated.View style={{ ...styles.purpleBox, opacity: opacity }} />
+        <Animated.View style={{ 
+          ...styles.purpleBox,
+          marginBottom: 20, 
+          opacity: opacity 
+        }} />
+        <Button 
+          title="FadeIn"
+          onPress={ fadeIn }
+        />
+        <Button 
+          title="FadeOut"
+          onPress={ fadeOut }
+        />
     </View>
   )
 }
