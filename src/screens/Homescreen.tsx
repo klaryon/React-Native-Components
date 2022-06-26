f50ec552eae3f578a3a16a97b3eb96c00b079b2e
Clarissa Ardon <klary.ardon@gmail.com>
2022-06-26 17:35:36 +0200

    initial switch component

--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -2,51 +2,42 @@ import React from 'react';
 import { View, FlatList, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { FlatListMenuItem } from '../components/FlatListMenuItem';
-import { MenuItem } from '../interfaces/appInterfaces';
+import { menuItems } from '../data/menuItems';
 import { styles } from '../theme/appTheme';
 
-const menuItems: MenuItem[] = [
-  {
-    name: 'Animation 101',
-    icon: 'cube-outline',
-    component: 'Animation101Screen'
-  },
-  {
-    name: 'Animation 102',
-    icon: 'albums-outline',
-    component: 'Animation102Screen'
-  }
-]
-
 const Homescreen = () => {
-
   const { top } = useSafeAreaInsets();
 
   const renderListHeader = () => {
     return (
       <View style={{ marginTop: top + 20, marginBottom: 20 }}>
-        <Text style={ styles.title }>Menu options</Text>
+        <Text style={styles.title}>Menu options</Text>
       </View>
-    )
-  }
+    );
+  };
 
   const itemSeparator = () => {
     return (
-      <View style={{ borderBottomWidth: 1, opacity: 0.4, marginVertical: 8 }}></View>
+      <View
+        style={{
+          borderBottomWidth: 1,
+          opacity: 0.4,
+          marginVertical: 8,
+        }}></View>
     );
-  }
+  };
 
   return (
     <View style={{ flex: 1, ...styles.globalMargin }}>
-      <FlatList 
-        data={ menuItems }
-        renderItem={ ({ item }) => <FlatListMenuItem menuItem={ item } /> }
-        keyExtractor={ (item) => item.name }
-        ListHeaderComponent={ renderListHeader }
-        ItemSeparatorComponent={ itemSeparator }
+      <FlatList
+        data={menuItems}
+        renderItem={({ item }) => <FlatListMenuItem menuItem={item} />}
+        keyExtractor={item => item.name}
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={itemSeparator}
       />
     </View>
-  )
-}
+  );
+};
 
 export default Homescreen;
