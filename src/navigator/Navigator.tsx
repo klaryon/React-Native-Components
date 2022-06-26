3071e663bf06c787883f96ca3065e7e6bc1dda48
Clarissa Ardon <klary.ardon@gmail.com>
2022-06-26 23:31:35 +0200

    export fix in HomeScreen

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Homescreen from '../screens/Homescreen';
+import { HomeScreen } from '../screens/HomeScreen';
 import { Animation101Screen } from '../screens/Animation101Screen';
 import { Animation102Screen } from '../screens/Animation102Screen';
 import { SwitchScreen } from '../screens/SwitchScreen';
@@ -14,7 +14,7 @@ export const Navigator = () => {
       screenOptions={{
         headerShown: false,
       }}>
-      <Stack.Screen name="HomeScreen" component={Homescreen} />
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
       <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
       <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, FlatList, Text } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { FlatListMenuItem } from '../components/FlatListMenuItem';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { menuItems } from '../data/menuItems';
 import { styles } from '../theme/appTheme';
 
-const Homescreen = () => {
+export const HomeScreen = () => {
   const itemSeparator = () => {
     return (
       <View
@@ -29,5 +29,3 @@ const Homescreen = () => {
     </View>
   );
 };
-
-export default Homescreen;
