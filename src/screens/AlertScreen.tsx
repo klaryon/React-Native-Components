8c0f9930e3edd2ab688b4cf352f88317aaeb069c
Clarissa Ardon <klary.ardon@gmail.com>
2022-06-27 18:41:07 +0200

    AlertScreen component

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Button, View, Alert } from 'react-native';
+
+import prompt from 'react-native-prompt-android';
+
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
@@ -23,10 +26,36 @@ export const AlertScreen = () => {
     );
   };
 
+  const showPrompt = () => {
+    prompt(
+      'Enter password',
+      'Enter your password to continue',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: password => console.log('OK Pressed, password: ' + password),
+        },
+      ],
+      {
+        type: 'secure-text',
+        cancelable: false,
+        defaultValue: 'test',
+        placeholder: 'placeholder',
+      },
+    );
+  };
+
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alerts" />
       <Button title="Show Alert" onPress={showAlert} />
+      <View style={{ marginVertical: 4 }}></View>
+      <Button title="Show Prompt" onPress={showPrompt} />
     </View>
   );
 };
